d5192fa137634e1ff01bc261590f9d6b2aac510a
trongpro-hub <trong03042011@gmail.com>
2026-05-03 01:07:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log("Server chạy ở port " + PORT);
 });
